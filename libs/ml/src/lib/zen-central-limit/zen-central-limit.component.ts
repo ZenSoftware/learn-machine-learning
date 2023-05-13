15217dd0c6b6e295bc3cd3074c6e6315aa27d397
Zen Software <ZenSoftware@users.noreply.github.com>
2023-05-13 12:47:40 -0600

    added zen-central-limit

---
+++ libs/ml/src/lib/zen-central-limit/zen-central-limit.component.ts
@@ -0,0 +1,59 @@
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, Point } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+@Component({
+  selector: 'zen-central-limit',
+  templateUrl: 'zen-central-limit.component.html',
+})
+export class ZenCentralLimitComponent {
+  @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
+
+  datasets: ChartConfiguration<'scatter'>['data']['datasets'] = [
+    {
+      data: [],
+      label: 'Sample',
+      pointRadius: 4,
+      backgroundColor: 'rgb(0,255,255)',
+    },
+  ];
+
+  options: ChartConfiguration<'scatter'>['options'] = {
+    responsive: false,
+    color: '#ffffff',
+  };
+
+  update() {
+    const results = new Map<number, number>();
+
+    for (let j = 0; j < 500; j++) {
+      let sum = 0;
+      for (let i = 0; i < 10000; i++) {
+        sum += this.getRndInteger(1, 6);
+      }
+
+      const val = results.get(sum);
+      if (val !== undefined) {
+        results.set(sum, val + 1);
+      } else {
+        results.set(sum, 1);
+      }
+    }
+
+    this.sampleData = Array.from(results.entries()).map(([x, y]) => ({ x, y }));
+
+    this.chart.render();
+  }
+
+  get sampleData() {
+    return (<any>this.datasets.find(x => x.label === 'Sample')).data as Point[];
+  }
+
+  set sampleData(data) {
+    (<any>this.datasets.find(x => x.label === 'Sample')).data = data;
+  }
+
+  getRndInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
--- libs/ml/src/lib/zen-ml.module.ts
+++ libs/ml/src/lib/zen-ml.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { NgChartsModule } from 'ng2-charts';
 
+import { ZenCentralLimitComponent } from './zen-central-limit/zen-central-limit.component';
 import { ZenLGComponent } from './zen-lg/zen-lg.component';
 import { ZenLocalLGComponent } from './zen-local-lg/zen-local-lg.component';
 import { ZenLogisticRegressionComponent } from './zen-logistic-regression/zen-logistic-regression.component';
@@ -12,6 +13,7 @@ import { ZenMLRoutingModule } from './zen-ml-routing.module';
 @NgModule({
   imports: [CommonModule, MatButtonModule, NgChartsModule, ZenMLRoutingModule],
   declarations: [
+    ZenCentralLimitComponent,
     ZenLGComponent,
     ZenLocalLGComponent,
     ZenLogisticRegressionComponent,
