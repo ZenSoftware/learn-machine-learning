8713f7aced1ae4352e3b838d3ae4674606c8e7b1
Zen Software <ZenSoftware@users.noreply.github.com>
2023-05-13 19:21:58 -0600

    fix(ml): simplified rng code
    undefined

--- libs/ml/src/lib/zen-lg/zen-lg.component.ts
+++ libs/ml/src/lib/zen-lg/zen-lg.component.ts
@@ -198,7 +198,7 @@ export class ZenLGComponent {
   }
 
   gaussianRandom(mean = 0, stdev = 1) {
-    const u = 1 - Math.random(); //Converting [0,1) to (0,1)
+    const u = Math.random();
     const v = Math.random();
     const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
     return z * stdev + mean;
